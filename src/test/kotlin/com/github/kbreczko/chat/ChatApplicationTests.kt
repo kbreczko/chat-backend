91e9121894ab1be5e2a5d85a6778df691e3fac63
kbreczko <kamil.breczko@gmail.com>
2019-01-27 18:47:37 +0100

    add spring boot

---
+++ src/test/kotlin/com/github/kbreczko/chat/ChatApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.github.kbreczko.chat
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
